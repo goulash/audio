d81d9b2256177d190be235d624f1c381a1891f7d
Ben Morgan <neembi@gmail.com>
2016-04-22 00:27:20 +0200

    flac: writing a few tests

--- flac/flac_test.go
+++ flac/flac_test.go
@@ -5,6 +5,7 @@
 package flac
 
 import (
+	"bytes"
 	"os"
 	"testing"
 
@@ -31,12 +32,60 @@ var testFileStreamInfo = &StreamInfo{
 	MD5Sum:        []byte("ce88fffba66d962c99bdd809c73d4d18"),
 }
 
+func TestReadStreamMarker(z *testing.T) {
+	tests := map[string]error{
+		"fLaC":    nil,
+		"FLAC":    ErrInvalidStream,
+		"fLaC...": nil,
+		"fLa":     ErrUnexpectedEOF,
+		"":        ErrUnexpectedEOF,
+	}
+
+	for k, v := range tests {
+		buf := bytes.NewBufferString(k)
+		err := readStreamMarker(buf)
+		if err != v {
+			z.Errorf("readStreamMarker(%q) = %v, expecting %v", k, err, v)
+		}
+	}
+}
+
+func TestReadBlockHeader(z *testing.T) {
+	assert := assert.New(z)
+	tests := []struct {
+		In   []byte
+		Out  uint32
+		Last bool
+		Type blockType
+		Size int64
+		Err  error
+	}{
+		{[]byte{0x0, 0x0, 0x0, 0x22}, 34, false, streamInfoBlock, 34, nil},
+		{[]byte{0x80, 0x0, 0x0, 0x0}, 2147483648, true, streamInfoBlock, 0, nil},
+		{[]byte{0x80, 0x0, 0x0}, 0, false, streamInfoBlock, 0, ErrUnexpectedEOF},
+	}
+
+	for _, t := range tests {
+		buf := bytes.NewBuffer(t.In)
+		h, err := readBlockHeader(buf)
+		assert.Equal(t.Out, uint32(h))
+		assert.Equal(t.Err, err)
+		assert.Equal(t.Last, h.IsLast())
+		assert.Equal(t.Type, h.Type())
+		assert.Equal(t.Size, h.Length())
+	}
+}
+
 func TestFile(z *testing.T) {
 	assert := assert.New(z)
 	f, err := os.Open(testFile)
-	assert.Nil(err)
+	if !assert.Nil(err) {
+		return
+	}
 	m, err := ReadMetadata(f)
-	assert.Nil(err)
+	if !assert.Nil(err) {
+		return
+	}
 	si, ti := testFileStreamInfo, m.StreamInfo()
 	assert.Equal(si.MinBlockSize, ti.MinBlockSize)
 	assert.Equal(si.MaxBlockSize, ti.MaxBlockSize)
